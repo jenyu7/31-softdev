1a3d3a2fa4d3a73ec7c4f58a0cab4afa767dfb16
Jen <jyu7@stuy.edu>
2018-04-27 00:34:01 -0400

    done

---
+++ math.py
@@ -0,0 +1,33 @@
+'''
+duckdo
+Jen Yu and Alessandro Cartegni
+2018-04-27
+K #16: Ready, Set, Math!
+Period 7 SoftDev 
+'''
+
+def intersection(a, b):
+    return [x for x in a if x in b]
+
+def union(a, b):
+    i = intersection(a, b)
+    return [x for x in a+b if x not in i] + i
+
+def set_diff(a, b):
+    return [x for x in a if x not in b]
+
+def sym_diff(a, b):
+    return set_diff(a, b) + set_diff(b, a)
+
+def cartesian(a, b):
+    return [[x, y] for x in a for y in b]
+
+a = [1, 2, 3, 4]
+b = [2, 4, 6, 8]
+
+
+print intersection(a, b) #[2, 4]
+print union(a, b) #[1, 2, 3, 4, 6, 8] (may be in different order)
+print set_diff(a, b) #[1, 3]
+print sym_diff(a, b) #[1, 3, 6, 8]
+print cartesian(a, b) #set of all possible combinations
